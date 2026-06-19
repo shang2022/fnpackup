ba446c63054035ef40fc43f2752aff37bf1f4192
snltty <iswwping@163.com>
2026-06-19 22:37:19 +0800

    index.cgi

--- shells/publish-fpk.sh
+++ shells/publish-fpk.sh
@@ -3,9 +3,9 @@ target=$(cd $(dirname $0)/..; pwd)
 mkdir -p public/publish-fpk/docker
 cp -rf install-package/fpk/docker/* public/publish-fpk/docker
 
-sed -i "s|{version}|1.1.3|g" public/publish-fpk/docker/manifest
-sed -i "s|{changelog}|1. 增加统一网关注册|g" public/publish-fpk/docker/manifest
-sed -i "s|{version}|v1.1.3|g" public/publish-fpk/docker/app/docker/docker-compose.yaml
+sed -i "s|{version}|1.1.4|g" public/publish-fpk/docker/manifest
+sed -i "s|{changelog}|1. index.cgi|g" public/publish-fpk/docker/manifest
+sed -i "s|{version}|v1.1.4|g" public/publish-fpk/docker/app/docker/docker-compose.yaml
 sed -i 's/\r$//' public/publish-fpk/docker/manifest
 sed -i 's/\r$//' public/publish-fpk/docker/cmd/main
 sed -i 's/\r$//' public/publish-fpk/docker/cmd/uninstall_callback
